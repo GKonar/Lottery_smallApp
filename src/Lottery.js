874a6d3c5f09119ad96175694328af036bf30400
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2019-05-26 10:16:07 +0200

    Add cleaner solution for generateNumbers method

--- src/Lottery.js
+++ src/Lottery.js
@@ -20,22 +20,12 @@ class Lottery extends Component {
 
 	generateNumbers() {
 		let winningNumbers = [];
-		for (let i = 0; i < this.props.numBalls; i++) {
+		while (winningNumbers.length < this.props.numBalls) {
 			let lotteryNum = Math.floor(Math.random() * this.props.maxNum) + 1;
 			if (winningNumbers.indexOf(lotteryNum) === -1) winningNumbers.push(lotteryNum);
-		}
-
-		if (winningNumbers.length === this.props.numBalls) {
 			this.setState({ numbers: winningNumbers });
-		} else {
-			// if array length is not equal props.numBalls call function again till U gonna get array with different random numbers
-			this.generateNumbers();
 		}
 	}
-	// second solution with repetitive numbers
-	// this.setState(curState => ({
-	// 	numbers: curState.numbers.map(n => Math.floor(Math.random() * this.props.maxNum) + 1)
-	// }));
 
 	handleClick() {
 		this.generateNumbers();
